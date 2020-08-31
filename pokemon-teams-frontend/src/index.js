c8c435320f9d51b19a575c052d64b09ca6bdd01c
redazures <jwu_nyc@yahoo.com>
2020-08-31 17:09:28 -0400

    added alert

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -17,12 +17,6 @@ function get(){
 function objectify(josh){ //console.log("you objectifying josh")
     josh.forEach(trainer=>render(trainer)) 
 }
-    
-    // 
-    //   <li>Zachariah (Ditto) <button class="release" data-pokemon-id="141">Release</button></li>
-    //   <li>Mittie (Farfetch'd) <button class="release" data-pokemon-id="149">Release</button></li>
-    //   <li>Rosetta (Eevee) <button class="release" data-pokemon-id="150">Release</button></li>
-    //   <li>Rod (Beedrill) <button class="release" data-pokemon-id="151">Release</button></li>
 
 function render(trainer){//console.log(`${trainer.id} ${trainer.name}`)
         
@@ -49,47 +43,51 @@ function render(trainer){//console.log(`${trainer.id} ${trainer.name}`)
 function clicker(){
     document.addEventListener('click',(e)=>{//console.log("i am clicking")
         if(e.target.matches('.add')){//console.log("this is the add button") //console.log(parseInt(e.target.dataset.trainerId))
-            const trainerId = parseInt(e.target.dataset.trainerId)
-            fetch('http://localhost:3000/pokemons', {
-            method: "POST",
-            headers: 
-            {
-                'Content-Type': 'application/json'
+            if(e.target.nextElementSibling.childElementCount>=6){alert("You already have 6 pokemon. You cannot add anymore")}else{
+                const trainerId = parseInt(e.target.dataset.trainerId)
+                fetch('http://localhost:3000/pokemons', {
+                method: "POST",
+                headers: 
+                {
+                    'Content-Type': 'application/json'
+                }
+                ,
+                body: JSON.stringify({
+                    "trainer_id":trainerId
+                })
+                })
+                .then(res => res.json())
+                .then(poke=>{
+                const pokecontainer = e.target.nextElementSibling //console.log(pokecontainer)
+                const list = document.createElement('li')
+                list.dataset.trainerId=`${trainerId}`
+                list.innerHTML=`${poke.nickname} ${poke.species} <button class="release" data-pokemon-id="${poke.id}">Release</button>`
+                pokecontainer.appendChild(list)
+                })
             }
-            ,
-            body: JSON.stringify({
-                "trainer_id":trainerId
-            })
-            })
-            .then(res => res.json())
-            .then(poke=>{
-            const pokecontainer = e.target.nextElementSibling //console.log(pokecontainer)
-            const list = document.createElement('li')
-            list.dataset.trainerId=`${trainerId}`
-            list.innerHTML=`${poke.nickname} ${poke.species} <button class="release" data-pokemon-id="${poke.id}">Release</button>`
-            pokecontainer.appendChild(list)
-            })
         }
         // //The below is the release button
         if(e.target.matches('.release')){//console.log("this is the release button")
             const pokeId = parseInt(e.target.dataset.pokemonId)
-            const trainerId = parseInt(e.target.parentElement.dataset.trainerId)
-            const nickname = e.target.parentElement.textContent.split(" ")[0]
-            const species = e.target.parentElement.textContent.split(" ")[1]
+            // const trainerId = parseInt(e.target.parentElement.dataset.trainerId)
+            // const nickname = e.target.parentElement.textContent.split(" ")[0]
+            // const species = e.target.parentElement.textContent.split(" ")[1]
 
-            faker ={
-                "id":pokeId,
-                "nickname":nickname,
-                "species":species,
-                "trainer_id":trainerId
-            }
+            // faker ={
+            //     "id":pokeId,
+            //     "nickname":nickname,
+            //     "species":species,
+            //     "trainer_id":trainerId
+            // }
 
-            console.log(faker)
+            // console.log(faker)
             fetch('http://localhost:3000/pokemons/'+pokeId,{
             method: "DELETE" 
-            }).then(resp => resp.url).then(console.log)
-
-            e.target.parentElement.remove()
+            }).then(resp => resp.json()).then(string =>{
+                console.log(string.id)
+                console.log(pokeId)
+                if (string.id==pokeId){e.target.parentElement.remove(),console.log("delete")}else{alert("Delete something wrong")}
+            })
         }
     })
 }
